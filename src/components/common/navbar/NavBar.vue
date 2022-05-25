a9e7bb5f59b5b111ba6ef8fae047dd1ae0cd98cc
unknown <dyj00112315a@vip.qq.com>
2022-05-25 11:39:30 +0800

    temp

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-bar">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {}
+}
+
+</script>
+<style  scoped lang="scss">
+</style>
